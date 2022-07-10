ac5aa564f7b90354fa36d246b331ddbe079d7619
carrebola <carrebola@fpllefia.com>
2022-07-10 16:30:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <VerMascotas/>
+</template>
+
+<script>
+import VerMascotas from './components/VerMascotas.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VerMascotas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VerMascotas.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <h1>Mascotas</h1>
+        <div v-for="item in arrayMascotas.datos" :key="item.nombre" class="card mb-3" style="max-width: 540px;">
+            <div  class="row g-0">
+                <div class="col-md-4">
+                <img :src=urlImagen(item.imagen) class="img-fluid rounded-start" alt="">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{item.nombre}}</h5>
+                    <p class="card-text">{{item.descripción}}</p>
+                    
+                </div>
+                </div>
+            </div>
+            </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                arrayMascotas: []
+            }
+        },
+        methods: {
+            urlImagen(imagen) {
+                return 'https://ejemplo-soc.herokuapp.com/imagenes/'+imagen 
+            },
+            async verMascotas() {
+                console.log('ver mascotas');
+                
+                try {
+                    const resultado = await fetch('https://ejemplo-soc.herokuapp.com/api/mascotas');
+                    const result = await resultado.json() 
+                    this.arrayMascotas = result
+                } catch (error) {
+                    console.log(error);
+                }
+                
+            }
+        },
+        computed: {
+            
+        },
+        mounted () {
+            this.verMascotas()
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
